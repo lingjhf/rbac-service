121445152f40c11d63558f05cd6f79dbaea5a2f8
lingj <lingj.jhf@outlook.com>
2023-03-29 00:35:09 +0800

    feat: 添加如果没有表就创建表功能

--- utils/utils.go
+++ utils/utils.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+
+	"gorm.io/gorm"
 )
 
 // Retry 重试函数
@@ -28,4 +30,16 @@ func Retry(fn func() error, n uint, d time.Duration) (err error) {
 // GenerateCaptcha 随机生成4位数验证码
 func GenerateCaptcha() string {
 	return fmt.Sprintf("%04v", rand.New(rand.NewSource(time.Now().UnixNano())).Int31n(10000))
+}
+
+// CreateTablesIfNotExists 判断表如果不存在就创建表
+func CreateTablesIfNotExists(db *gorm.DB, tables ...interface{}) error {
+	for _, table := range tables {
+		if !db.Migrator().HasTable(table) {
+			if err := db.Migrator().CreateTable(table); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
