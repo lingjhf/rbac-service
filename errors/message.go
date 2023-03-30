e52296359853810d6ec17712c91b6a4348cb0b8c
lingj <lingj.jhf@outlook.com>
2023-03-31 00:38:52 +0800

    chore: 修改响应格式

--- errors/message.go
+++
@@ -1,6 +0,0 @@
-package errors
-
-const (
-	Unknown             = "未知错误"
-	CreateUserSucceeded = "创建用户成功"
-)
--- errors/response.go
+++ errors/response.go
@@ -1,7 +1,73 @@
 package errors
 
+import "github.com/gofiber/fiber/v2"
+
 type JSONResponse struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
+	Code    string `json:"code"`
+	Message any    `json:"message"`
 	Data    any    `json:"data"`
+}
+
+func Succeeded(c *fiber.Ctx) error {
+	return c.JSON(&JSONResponse{
+		Code:    ErrSucceeded.Code,
+		Message: ErrSucceeded.Message,
+	})
+}
+
+func SucceededWithData(c *fiber.Ctx, data any) error {
+	return c.JSON(&JSONResponse{
+		Code:    ErrSucceeded.Code,
+		Message: ErrSucceeded.Message,
+		Data:    data,
+	})
+}
+
+func BodyParserError(c *fiber.Ctx, message any) error {
+	return c.JSON(&JSONResponse{
+		Code:    ErrBodyParser.Code,
+		Message: message,
+	})
+}
+
+func ParameterError(c *fiber.Ctx, message any) error {
+	return c.JSON(&JSONResponse{
+		Code:    ErrParameter.Code,
+		Message: message,
+	})
+}
+
+func DatabaseError(c *fiber.Ctx) error {
+	return c.JSON(&JSONResponse{
+		Code:    ErrDatabase.Code,
+		Message: ErrDatabase.Message,
+	})
+}
+
+func SignupError(c *fiber.Ctx) error {
+	return c.JSON(&JSONResponse{
+		Code:    ErrSignin.Code,
+		Message: ErrSignin.Message,
+	})
+}
+
+func SigninError(c *fiber.Ctx) error {
+	return c.JSON(&JSONResponse{
+		Code:    ErrSignin.Code,
+		Message: ErrSignin.Message,
+	})
+}
+
+func UnauthorizedError(c *fiber.Ctx) error {
+	return c.JSON(&JSONResponse{
+		Code:    ErrUnauthorized.Code,
+		Message: ErrUnauthorized.Message,
+	})
+}
+
+func TokenExpiredError(c *fiber.Ctx) error {
+	return c.JSON(&JSONResponse{
+		Code:    ErrTokenExpired.Code,
+		Message: ErrTokenExpired.Message,
+	})
 }
