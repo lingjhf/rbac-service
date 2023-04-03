d890a59b30e97db77590e65e00669ca9a294cd58
lingj <lingj.jhf@outlook.com>
2023-04-04 03:32:28 +0800

    chore: 修改表结构，表名称，字段名称

--- tables/role.go
+++ tables/role.go
@@ -2,19 +2,19 @@ package tables
 
 type Role struct {
 	Base
-	Name      string  `gorm:"type:varchar(32);not null;"`
-	TenancyId string  `gorm:"type:varchar(36);not null;"`
-	Tenancy   Tenancy `gorm:"foreignKey:Id;references:TenancyId;constraint:OnDelete:CASCADE;"`
+	Name     string `gorm:"type:varchar(32);not null;"`
+	TenantId string `gorm:"type:varchar(36);not null;"`
+	Tenant   Tenant `gorm:"foreignKey:Id;references:TenantId;constraint:OnDelete:CASCADE;"`
 }
 
 type RolePermission struct {
 	Base
 	RoleId       string      `gorm:"type:varchar(36);not null;uniqueIndex:idx_role_permission;"`
 	PermissionId string      `gorm:"type:varchar(36);not null;uniqueIndex:idx_role_permission;"`
-	TenancyId    string      `gorm:"type:varchar(36);not null;uniqueIndex:idx_role_permission;"`
+	TenantId     string      `gorm:"type:varchar(36);not null;uniqueIndex:idx_role_permission;"`
 	Role         *Role       `gorm:"foreignKey:Id;references:RoleId;constraint:OnDelete:CASCADE;"`
 	Permission   *Permission `gorm:"foreignKey:Id;references:PermissionId;constraint:OnDelete:CASCADE;"`
-	Tenancy      Tenancy     `gorm:"foreignKey:Id;references:TenancyId;constraint:OnDelete:CASCADE;"`
+	Tenant       Tenant      `gorm:"foreignKey:Id;references:TenantId;constraint:OnDelete:CASCADE;"`
 }
 
 func (Role) TableName() string {
--- tables/user.go
+++ tables/user.go
@@ -8,22 +8,22 @@ type User struct {
 	Phone    string `gorm:"type:varchar(20);index;not null;default:'';"`
 }
 
-type UserTenancy struct {
+type UserTenant struct {
 	Base
-	UserId    string  `gorm:"type:varchar(36);not null;uniqueIndex:idx_user_tenancy;"`
-	TenancyId string  `gorm:"type:varchar(36);not null;uniqueIndex:idx_user_tenancy;"`
-	User      User    `gorm:"foreignKey:Id;references:UserId;constraint:OnDelete:CASCADE;"`
-	Tenancy   Tenancy `gorm:"foreignKey:Id;references:TenancyId;constraint:OnDelete:CASCADE;"`
+	UserId   string `gorm:"type:varchar(36);not null;uniqueIndex:idx_user_tenant;"`
+	TenantId string `gorm:"type:varchar(36);not null;uniqueIndex:idx_user_tenant;"`
+	User     User   `gorm:"foreignKey:Id;references:UserId;constraint:OnDelete:CASCADE;"`
+	Tenant   Tenant `gorm:"foreignKey:Id;references:TenantId;constraint:OnDelete:CASCADE;"`
 }
 
 type UserRole struct {
 	Base
-	UserId    string  `gorm:"type:varchar(36);not null;uniqueIndex:idx_user_role;"`
-	RoleId    string  `gorm:"type:varchar(36);not null;uniqueIndex:idx_user_role;"`
-	TenancyId string  `gorm:"type:varchar(36);not null;uniqueIndex:idx_user_role;"`
-	User      User    `gorm:"foreignKey:Id;references:UserId;constraint:OnDelete:CASCADE;"`
-	Role      Role    `gorm:"foreignKey:Id;references:RoleId;constraint:OnDelete:CASCADE;"`
-	Tenancy   Tenancy `gorm:"foreignKey:Id;references:TenancyId;constraint:OnDelete:CASCADE;"`
+	UserId   string `gorm:"type:varchar(36);not null;uniqueIndex:idx_user_role;"`
+	RoleId   string `gorm:"type:varchar(36);not null;uniqueIndex:idx_user_role;"`
+	TenantId string `gorm:"type:varchar(36);not null;uniqueIndex:idx_user_role;"`
+	User     User   `gorm:"foreignKey:Id;references:UserId;constraint:OnDelete:CASCADE;"`
+	Role     Role   `gorm:"foreignKey:Id;references:RoleId;constraint:OnDelete:CASCADE;"`
+	Tenant   Tenant `gorm:"foreignKey:Id;references:TenantId;constraint:OnDelete:CASCADE;"`
 }
 
 func (User) TableName() string {
@@ -34,6 +34,6 @@ func (UserRole) TableName() string {
 	return "rbac_user_role"
 }
 
-func (UserTenancy) TableName() string {
-	return "rbac_user_tenancy"
+func (UserTenant) TableName() string {
+	return "rbac_user_tenant"
 }
