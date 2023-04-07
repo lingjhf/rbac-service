de894f48d9270084b1d57dcd513fcffeb6462b24
lingj <lingj.jhf@outlook.com>
2023-04-07 16:05:00 +0800

    chore: 添加数据接口

--- services/permission.go
+++ services/permission.go
@@ -2,24 +2,149 @@ package services
 
 import (
 	"rbac-service/errors"
+	"rbac-service/models"
+	"rbac-service/tables"
+	"rbac-service/utils"
 
 	"github.com/gofiber/fiber/v2"
 )
 
+const (
+	CreatePermissionRouteName  = "permission.create_permission"
+	GetPermissionListRouteName = "permission.get_permission_list"
+	UpdatePermissionRouteName  = "permission.update_permission"
+	GetPermissionByIdRouteName = "permission.get_permission_by_id"
+)
+
 func (s *Service) NewPermissionService() Servicer {
 	s.App.Route("/permission", func(router fiber.Router) {
-		router.Get("/:id", s.GetPermissionById).Name("get_permission_by_id")
-		router.Post("/create", s.CreatePermission).Name("create_permission")
-	}, "permission.")
+		router.Use(s.RequiredSignin)
+		router.Post("/create", s.RequiredPermission, s.CreatePermission).Name(CreatePermissionRouteName)
+		router.Get("/list", s.RequiredPermission, s.GetPermissionList).Name(GetPermissionListRouteName)
+		router.Put("/:id", s.RequiredPermission, s.UpdatePermission).Name(UpdatePermissionRouteName)
+		router.Get("/:id", s.RequiredPermission, s.GetPermissionById).Name(GetPermissionByIdRouteName)
+	})
 	return s
 }
 
-func (s *Service) GetPermissionById(c *fiber.Ctx) error {
-
-	return c.JSON(&errors.JSONResponse{})
+// CreatePermission 创建权限
+// 只能创建当前租户下的权限
+func (s *Service) CreatePermission(c *fiber.Ctx) error {
+	form := &models.CreatePermissionForm{}
+	if err := c.BodyParser(form); err != nil {
+		return errors.BodyParserError(c, err.Error())
+	}
+	if err := form.Validate(); err != nil {
+		return errors.ParameterError(c, err)
+	}
+	tenantId := c.UserContext().Value(ContextKey("tenantId")).(string)
+	permissionExists, err := s.Dao.GetPermissionByName(form.Name, tenantId)
+	if err != nil {
+		return errors.DatabaseError(c)
+	}
+	if permissionExists != nil {
+		return errors.ParameterError(c, "权限名称已存在")
+	}
+	permissionExists, err = s.Dao.GetPermissionByCode(form.Code, tenantId)
+	if err != nil {
+		return errors.DatabaseError(c)
+	}
+	if permissionExists != nil {
+		return errors.ParameterError(c, "权限码已存在")
+	}
+	permission := &tables.Permission{Name: form.Name, Code: form.Code, Description: form.Description, TenantId: tenantId}
+	permission.Init()
+	if err := s.Dao.CreatePermission(permission); err != nil {
+		return errors.DatabaseError(c)
+	}
+	return errors.Succeeded(c)
 }
 
-func (s *Service) CreatePermission(c *fiber.Ctx) error {
+func (s *Service) UpdatePermission(c *fiber.Ctx) error {
+	permissionId := c.Params("id")
+	tenantId := c.UserContext().Value(ContextKey("tenantId")).(string)
+	permissionExists, err := s.Dao.GetPermissionById(permissionId, tenantId)
+	if err != nil {
+		return errors.DatabaseError(c)
+	}
+	if permissionExists == nil {
+		return errors.ParameterError(c, "权限不存在")
+	}
+	form := models.UpdatePermissionForm{}
+	if err := c.BodyParser(&form); err != nil {
+		return errors.BodyParserError(c, err.Error())
+	}
+	if err := form.Validate(); err != nil {
+		return errors.ParameterError(c, err)
+	}
+	updateMap := map[string]any{}
+	if name, ok := form["name"]; ok && name != permissionExists.Name {
+		permissionExists, err := s.Dao.GetPermissionByName(name.(string), tenantId)
+		if err != nil {
+			return errors.DatabaseError(c)
+		}
+		if permissionExists != nil {
+			return errors.ParameterError(c, "权限名称已存在")
+		}
+		updateMap["name"] = name
+	}
+	if code, ok := form["code"]; ok && code != permissionExists.Code {
+		permissionExists, err := s.Dao.GetPermissionByCode(code.(string), tenantId)
+		if err != nil {
+			return errors.DatabaseError(c)
+		}
+		if permissionExists != nil {
+			return errors.ParameterError(c, "权限码已存在")
+		}
+		updateMap["code"] = code
+	}
+	if description, ok := form["description"]; ok && description != permissionExists.Description {
+		updateMap["description"] = description
+	}
+	if err := s.Dao.UpdatePermission(permissionExists, updateMap); err != nil {
+		return errors.DatabaseError(c)
+	}
+	return errors.Succeeded(c)
+}
 
-	return c.JSON(&errors.JSONResponse{})
+func (s *Service) GetPermissionById(c *fiber.Ctx) error {
+	permissionId := c.Params("id")
+	tenantId := c.UserContext().Value(ContextKey("tenantId")).(string)
+	permissionExists, err := s.Dao.GetPermissionById(permissionId, tenantId)
+	if err != nil {
+		return errors.DatabaseError(c)
+	}
+	if permissionExists == nil {
+		return errors.ParameterError(c, "权限不存在")
+	}
+	return errors.SucceededWithData(c, map[string]any{
+		"id":   permissionExists.Id,
+		"name": permissionExists.Name,
+		"code": permissionExists.Code,
+	})
+}
+
+func (s *Service) GetPermissionList(c *fiber.Ctx) error {
+	query := &models.PermissionQuery{}
+	if err := c.QueryParser(query); err != nil {
+		return errors.QueryParserError(c, err.Error())
+	}
+	tenantId := c.UserContext().Value(ContextKey("tenantId")).(string)
+	offset, limit := utils.GetOffsetLimit(query.Page, query.Limit)
+	permissionList, err := s.Dao.GetPermissionList(
+		tenantId,
+		offset,
+		limit,
+	)
+	if err != nil {
+		return errors.DatabaseError(c)
+	}
+	count, err := s.Dao.GetPermissionCount(tenantId)
+	if err != nil {
+		return errors.DatabaseError(c)
+	}
+	return errors.SucceededWithData(c, map[string]any{
+		"list":  new(models.PermissionList).FormTable(permissionList),
+		"total": count,
+	})
 }
--- services/tenancy.go
+++ services/tenancy.go
@@ -2,24 +2,195 @@ package services
 
 import (
 	"rbac-service/errors"
+	"rbac-service/models"
+	"rbac-service/tables"
+	"rbac-service/utils"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-func (s *Service) NewTenancyService() Servicer {
-	s.App.Route("/tenancy", func(router fiber.Router) {
-		router.Get("/:id", s.GetTenancyById).Name("get_tenancy_by_id")
-		router.Post("/create", s.CreateTenancy).Name("create_tenancy")
-	}, "tenancy.")
+const (
+	CreateTenantRouteName  = "tenant.create_tenant"
+	GetTenantListRouteName = "tenant.get_tenant_list"
+	UpdateTenantRouteName  = "tenant.update_tenant"
+	GetTenantByIdRouteName = "tenant.get_tenant_by_id"
+)
+
+func (s *Service) NewTenantService() Servicer {
+	s.App.Route("/tenant", func(router fiber.Router) {
+		router.Use(s.RequiredSignin)
+		router.Post("/create", s.RequiredPermission, s.CreateTenant).Name(CreateTenantRouteName)
+		router.Get("/list", s.RequiredPermission, s.GetTenantList).Name(GetTenantListRouteName)
+		router.Put("/:id", s.RequiredPermission, s.UpdateTenant).Name(UpdateTenantRouteName)
+		router.Get("/:id", s.RequiredPermission, s.GetTenantById).Name(GetTenantByIdRouteName)
+	})
 	return s
 }
 
-func (s *Service) GetTenancyById(c *fiber.Ctx) error {
-
-	return c.JSON(&errors.JSONResponse{})
+// CreateTenant 创建租户
+// 判断创建的租户是否有父级id，如果有需要判断父级id是否属于上下文tenantId的子级
+// 判断租户名称是否已经存在
+func (s *Service) CreateTenant(c *fiber.Ctx) error {
+	form := &models.CreateTenantForm{}
+	if err := c.BodyParser(form); err != nil {
+		return errors.BodyParserError(c, err.Error())
+	}
+	if err := form.Validate(); err != nil {
+		return errors.ParameterError(c, err)
+	}
+	user := c.UserContext().Value(ContextKey("user")).(*tables.User)
+	if form.ParentId != nil {
+		tenantId, ok := c.UserContext().Value(ContextKey("tenantId")).(string)
+		if !ok {
+			return errors.UnauthorizedError(c)
+		}
+		tenantTreeExists, err := s.Dao.GetTenantTree(tenantId, *form.ParentId)
+		if err != nil {
+			return errors.DatabaseError(c)
+		}
+		if tenantTreeExists == nil {
+			return errors.UnauthorizedError(c)
+		}
+	}
+	tenantExists, err := s.Dao.GetTenantByNameWithParent(form.Name, form.ParentId)
+	if err != nil {
+		return errors.DatabaseError(c)
+	}
+	if tenantExists != nil {
+		return errors.ParameterError(c, "租户已存在")
+	}
+	tenant := &tables.Tenant{Name: form.Name, ParentId: form.ParentId, Owner: user.Id}
+	tenant.Init()
+	if err := s.Dao.CreateTenant(tenant); err != nil {
+		return errors.DatabaseError(c)
+	}
+	return errors.Succeeded(c)
 }
 
-func (s *Service) CreateTenancy(c *fiber.Ctx) error {
+// UpdateTenant 更新租户
+func (s *Service) UpdateTenant(c *fiber.Ctx) error {
+	updateTenantId := c.Params("id")
+	tenantId := c.UserContext().Value(ContextKey("tenantId")).(string)
+	originTenant, err := s.Dao.GetTenantJoinTenantTreeOnDescendant(tenantId, updateTenantId)
+	if err != nil {
+		return errors.DatabaseError(c)
+	}
+	if originTenant == nil {
+		return errors.UnauthorizedError(c)
+	}
+	form := models.UpdateTenantForm{}
+	if err := c.BodyParser(&form); err != nil {
+		return errors.BodyParserError(c, err.Error())
+	}
+	if err := form.Validate(); err != nil {
+		return errors.ParameterError(c, err)
+	}
+	updateMap := map[string]any{}
+	if err := s.CompareName(c, form, originTenant, updateMap); err != nil {
+		return err
+	}
+	if err := s.CompareParentId(c, form, tenantId, originTenant, updateMap); err != nil {
+		return err
+	}
+	if len(updateMap) > 0 {
+		if err := s.Dao.UpdateTenant(originTenant, updateMap); err != nil {
+			return errors.DatabaseError(c)
+		}
+	}
+	return errors.Succeeded(c)
+}
 
-	return c.JSON(&errors.JSONResponse{})
+func (s *Service) CompareName(c *fiber.Ctx, form models.UpdateTenantForm, originTenant *tables.Tenant, updateMap models.UpdateMap) error {
+	if name, ok := form["name"]; ok && name != originTenant.Name {
+		tenantExists, err := s.Dao.GetTenantByNameWithParent(name.(string), originTenant.ParentId)
+		if err != nil {
+			return errors.DatabaseError(c)
+		}
+		if tenantExists != nil {
+			return errors.ParameterError(c, "租户已存在")
+		}
+		updateMap["name"] = name
+	}
+	return nil
+}
+
+func (s *Service) CompareParentId(c *fiber.Ctx, form models.UpdateTenantForm, contextTenantId string, originTenant *tables.Tenant, updateMap models.UpdateMap) error {
+	if parentId, ok := form["parent_id"]; ok && parentId != originTenant.ParentId {
+		if parentId != nil {
+			tenantTreeExists, err := s.Dao.GetTenantTree(contextTenantId, parentId.(string))
+			if err != nil {
+				return errors.DatabaseError(c)
+			}
+			if tenantTreeExists == nil {
+				return errors.UnauthorizedError(c)
+			}
+		}
+		var tenantExists *tables.Tenant
+		var err error
+		parentIdString, isString := parentId.(string)
+		if name, ok := form["name"]; ok && name != originTenant.Name {
+			if isString {
+				tenantExists, err = s.Dao.GetTenantByNameWithParent(name.(string), &parentIdString)
+			} else {
+				tenantExists, err = s.Dao.GetTenantByNameWithParent(name.(string), nil)
+			}
+		} else {
+			if isString {
+				tenantExists, err = s.Dao.GetTenantByNameWithParent(originTenant.Name, &parentIdString)
+			} else {
+				tenantExists, err = s.Dao.GetTenantByNameWithParent(originTenant.Name, nil)
+			}
+		}
+		if err != nil {
+			return errors.DatabaseError(c)
+		}
+		if tenantExists != nil {
+			return errors.ParameterError(c, "租户已存在")
+		}
+		updateMap["parent_id"] = parentId
+	}
+	return nil
+}
+
+// GetTenantById 通过id获取租户
+func (s *Service) GetTenantById(c *fiber.Ctx) error {
+	tenantId := c.Params("id")
+	user := c.UserContext().Value(ContextKey("user")).(*tables.User)
+	tenantExists, err := s.Dao.GetTenantByIdWithOwner(tenantId, user.Id)
+	if err != nil {
+		return errors.DatabaseError(c)
+	}
+	if tenantExists == nil {
+		return errors.ParameterError(c, "租户不存在")
+	}
+	return errors.SucceededWithData(c, map[string]any{
+		"id":   tenantExists.Id,
+		"name": tenantExists.Name,
+	})
+}
+
+// GetTenantList 获取租户列表
+func (s *Service) GetTenantList(c *fiber.Ctx) error {
+	query := &models.TenantQuery{}
+	if err := c.QueryParser(query); err != nil {
+		return errors.QueryParserError(c, err.Error())
+	}
+	user := c.UserContext().Value(ContextKey("user")).(*tables.User)
+	offset, limit := utils.GetOffsetLimit(query.Page, query.Limit)
+	tenantList, err := s.Dao.GetTenantListByOwner(
+		user.Id,
+		offset,
+		limit,
+	)
+	if err != nil {
+		return errors.DatabaseError(c)
+	}
+	count, err := s.Dao.GetTenantCountByOwner(user.Id)
+	if err != nil {
+		return errors.DatabaseError(c)
+	}
+	return errors.SucceededWithData(c, map[string]any{
+		"list":  new(models.TenantList).FormTable(tenantList),
+		"total": count,
+	})
 }
