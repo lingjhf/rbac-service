7ea515c7227e0071832088c3a70ef963d4ff8056
lingj <lingj.jhf@outlook.com>
2023-04-09 22:58:42 +0800

    chore: 添加signup页面

--- ui/src/pages/signup/signup.vue
+++ ui/src/pages/signup/signup.vue
@@ -1,7 +1,34 @@
 <template>
-  <div />
+  <div class="flex h-screen items-center justify-center">
+    <el-form label-position="top" :model="form">
+      <el-form-item label="用户名">
+        <el-input v-model="form.username" />
+      </el-form-item>
+      <el-form-item label="邮箱">
+        <el-input v-model="form.email" />
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="form.password" />
+      </el-form-item>
+      <el-button type="primary" class="w-full" @click="onSignup">
+        注册
+      </el-button>
+    </el-form>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ElForm, ElFormItem, ElInput, ElButton } from 'element-plus'
+import { ref } from 'vue'
+import * as api from '@/api'
 
-<style scoped></style>
+const form = ref<api.SignupWithEmailData>({
+  username: '',
+  email: '',
+  password: '',
+})
+
+async function onSignup() {
+  await api.Signup('email', form.value)
+}
+</script>
